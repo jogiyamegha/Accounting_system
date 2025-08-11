ded70efbfc80d4dcc55a6be930cd236835409e8e
urmikj628 <urmikurmik861@gmail.com>
2025-08-11 12:21:18 +0530

    updated

---
+++ src/emails/email.js
@@ -0,0 +1,104 @@
+const Handlebars = require("handlebars");
+const { TableFields, GeneralMessages } = require("../utils/constants");
+const path = require("path");
+const fs = require("fs");
+const customerViewDirPath = path.join(__dirname, "../templates");
+const nodemailer = require("nodemailer");
+
+exports.sendForgotPasswordMail = async (emailId, code) => {
+  const resetPasswordTemplate = fs
+    .readFileSync(
+      path.join(customerViewDirPath, "admin", "forgot-password.hbs")
+    )
+    .toString();
+
+  let data = {
+    code: code,
+  };
+
+  const template = Handlebars.compile(resetPasswordTemplate);
+  try {
+    await sendEmail(
+      emailId,
+      GeneralMessages.forgotPasswordEmailSubject,
+      template(data)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+exports.sendClientResetCode = async (name, emailId, code) => {
+  const resetPasswordTemplate = fs
+    .readFileSync(
+      path.join(customerViewDirPath, "client", "forgot-password.hbs")
+    )
+    .toString();
+  let data = {
+    name: name,
+    code: code,
+  };
+  const template = Handlebars.compile(resetPasswordTemplate);
+
+  try {
+    await sendEmail(
+      emailId,
+      GeneralMessages.forgotPasswordEmailSubject,
+      template(data)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+exports.sendClientSignupMail = async (name, emailId, password) => {
+  const signupTemplate = fs
+    .readFileSync(path.join(customerViewDirPath, "client", "signup.hbs"))
+    .toString();
+
+  let data = {
+    name: name,
+    username: emailId,
+    password: password
+  };
+  const template = Handlebars.compile(signupTemplate);
+
+  try {
+    await sendEmail(emailId, GeneralMessages.signupEmailSucess, template(data));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function sendEmail(
+  receiverEMail,
+  subject,
+  htmlBodyContents,
+  fromAddress = process.env.SMTP_SENDER_EMAIL
+) {
+  let transporter = getTransportInfo();
+  let mailOptions = {
+    from: fromAddress,
+    to: receiverEMail,
+    subject: subject,
+    html: htmlBodyContents,
+  };
+
+  if (process.env.diasbleEmail == true || process.env.diasbleEmail == "true") {
+    return;
+  }
+
+  await transporter.sendMail(mailOptions);
+}
+
+function getTransportInfo() {
+  return nodemailer.createTransport({
+    host: process.env.SMTP_SERVER,
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+}
